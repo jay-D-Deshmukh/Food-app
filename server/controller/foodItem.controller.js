32a49e596ec6bdd7790c1982e890957ecbeff3c8
ShravanDeshmukh2305 <jaydeshmukh1212@gmail.com>
2024-04-24 20:27:03 +0530

    importing tha data of food item

--- server/controller/foodItem.controller.js
+++ server/controller/foodItem.controller.js
@@ -2,4 +2,15 @@ import foodItem from "../model/fooditem.mode.js";
 
 export const getFoodItems = async (req ,res)=>{
    
+    try {
+        // Fetch all cities from the database
+        const data = await foodItem.find();
+    
+        // Send the cities data as a response
+        res.json(data);
+      } catch (error) {
+        console.error('Error fetching cities:', error);
+        // Send an error response if something goes wrong
+        res.status(500).json({ error: 'Server error' });
+      }
 }
