242f11b2188106609909478b6592355147ed5751
jay-D-Deshmukh <jaydeshmukh1212@gmail.com>
2024-04-29 19:54:41 +0530

    creating the register testorent

---
+++ server/controller/restorent.controller.js
@@ -0,0 +1,15 @@
+import Restorent from "../model/restorent.mode.js";
+
+export const addReatorent =async (req, res)=>{
+    try {
+        
+        
+    } catch (error) {
+
+        console.log(res.status(404).json({message: error.message}))
+
+        
+        
+    }
+
+}
